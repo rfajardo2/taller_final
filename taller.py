e210da70a0b7fde221c14fb39e57ade4574457bd
Desarrollo <Desarrollo@DESKTOP-F722E5D>
2021-11-26 21:47:22 -0500

    arreglar data set

--- taller.py
+++ taller.py
@@ -17,8 +17,18 @@ data.shape
 # columnas del data set
 data.columns
 
+data.groupby('Estado').size()
+data.Sexo.replace('f','F',inplace=True)
+data.Sexo.replace('m','M',inplace=True)
+
+data.groupby('Sexo').size()
+data.Estado.replace('leve','Leve',inplace=True)
+data.Estado.replace('LEVE','Leve',inplace=True)
+
+
 # Realice las líneas de código en Python que den respuesta a las siguientes preguntas:
 # 1. Número de casos de Contagiados en el País.
 data.shape[0]
 
-
+# 2. Número de Municipios Afectados
+data.shape[0]
