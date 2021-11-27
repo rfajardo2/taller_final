6e704f230051829b6c8169264360a6689a4aa331
Desarrollo <Desarrollo@DESKTOP-F722E5D>
2021-11-26 21:38:58 -0500

    Actualizar .gitignore y taller.py

--- taller.py
+++ taller.py
@@ -5,3 +5,13 @@ Created on Fri Nov 26 19:53:48 2021
 @author: Desarrollo         
 """
 
+
+
+# columnas del data set
+data.columns
+
+# Realice las líneas de código en Python que den respuesta a las siguientes preguntas:
+# 1. Número de casos de Contagiados en el País.
+data.shape[0]
+
+
